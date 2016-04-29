819ab6663c04edf4cb5aa4e75d62c13879cdda6c
JustinFokes <justinfokes@Justins-MacBook-Pro.local>
2016-04-29 08:59:25 -0700

    make definition class fully tested, and fully functional

--- src/test/java/DefinitionTest.java
+++ src/test/java/DefinitionTest.java
@@ -3,10 +3,39 @@ import static org.junit.Assert.*;
 
 public class DefinitionTest {
 
-  // @Test
-  // public void methodWeAreTesting_behaviorWeAreTesting_expectedResult() {
-  //   MainClassName testMainClassName = new MainClassName();
-  //   String expectedResult = "This" ;
-  //   assertEquals(expectedResult, testMainClassName.methodToTest("test input"));
-  // }
+  @Test
+  public void Definition_instantiatesCorrectly_true() {
+    Definition definition = new Definition("the way things are");
+    assertEquals(true, definition instanceof Definition);
+  }
+  @Test
+  public void Definition_instantiatesWithDefinition_String() {
+    Definition definition = new Definition("the way things are");
+    assertEquals("the way things are", definition.getDefinition());
+  }
+  @Test
+  public void all_returnsAllInstancesOfDefinition_true(){
+   	Definition definitionOne = new Definition("the way things are");
+   	Definition definitionTwo = new Definition("the way things are supposed to be");
+    assertTrue(Definition.all().contains(definitionOne));
+    assertTrue(Definition.all().contains(definitionTwo));
+  }
+  @Test
+  public void clear_emptiesAllDefinitionFromArrayList_0() {
+    Definition definition = new Definition("the way things are");
+    Definition.clear();
+    assertEquals(Definition.all().size(), 0);
+  }
+  @Test
+  public void getId_DefinitionInstantiatesWithAnId_1(){
+	Definition.clear();
+	Definition definition = new Definition("the way things are");
+	assertEquals((Integer)1, definition.getId());
+  }
+  @Test
+  public void find_returnsDefinitionWithSameId_definitionTwo() {
+  	Definition definitionOne = new Definition("the way things are");
+   	Definition definitionTwo = new Definition("the way things are supposed to be");
+  	assertEquals(Definition.find(definitionTwo.getId()), definitionTwo);
+  }
 }
